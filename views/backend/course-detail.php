2d0092d630711615e7f4cd590e19adf5c69c128f
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2022-07-08 12:15:34 -0500

    Modificado numeracion

--- views/backend/course-detail.php
+++ views/backend/course-detail.php
@@ -33,6 +33,7 @@ $total_pending = 0;
     <div style="overflow-x: auto;">
         <table class="dcms-table-report dcms-table-details striped">
             <tr>
+                <th></th>
                 <th>Nombre</th>
                 <th>#Orden</th>
                 <th>Total</th>
@@ -40,13 +41,14 @@ $total_pending = 0;
                 <th>Pendiente</th>
                 <th>Flexible</th>
             </tr>
-            <?php foreach ($items as $item): ?>
+            <?php foreach ($items as $key => $item): ?>
                 <?php
                     $item_total += $item['item_total'];
                     $total_paid += $item['total_paid'];
                     $total_pending += $item['total_pending'];
                 ?>
                 <tr>
+                    <td><?= $key + 1 ?></td>
                     <td><?= $item['user_name'] . ' ' . $item['user_lastname'] ?></td>
                     <td>
                     <?= Helper::generate_order_links( $item['order_id'] ); ?>
@@ -59,6 +61,7 @@ $total_pending = 0;
             <?php endforeach; ?>
             <tfoot>
                 <tr>
+                    <td></td>
                     <td>Totales</td>
                     <td></td>
                     <td><?= number_format($item_total, 2) ?></td>
